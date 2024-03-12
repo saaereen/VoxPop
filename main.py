044df36bb488db803dae180505fb6c2e79263431
Shyryn Amangeldi <137295870+saaereen@users.noreply.github.com>
2024-03-12 16:44:37 +0600

    POST / route

--- main.py
+++ main.py
@@ -17,3 +17,9 @@ opinions = []
 @app.get("/", response_class=HTMLResponse)
 async def read_opinions(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/", response_class=RedirectResponse)
+async def create_opinion(request: Request, text: str = Form(...), category: str = Form(...), user: str = Form(...)):
+    opinion = Opinion(text=text, category=category, user=user)
+    opinions.insert(0, opinion)
+    return RedirectResponse(url="/opinions", status_code=303)
