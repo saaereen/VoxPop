6b5272dc330f35e207739fa19b4ad55cc4dfa270
Shyryn Amangeldi <137295870+saaereen@users.noreply.github.com>
2024-03-12 16:43:52 +0600

    GET / route

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from typing import List
 
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+class Opinion(BaseModel):
+    text: str
+    category: str
+    user: str
+
+opinions = []
+
+@app.get("/", response_class=HTMLResponse)
+async def read_opinions(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
